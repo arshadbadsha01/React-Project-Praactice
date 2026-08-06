0d6ac1c03b15c99e0904e64ed16bd0ae2d78ba9c
arshadbadsha01 <arshad.badsha01@gmail.com>
2026-08-06 21:29:58 +0530

    fix CORS origin

--- calculator-app/backend/app.js
+++ calculator-app/backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://react-project-practice-pi.vercel.app/",
+    origin: "https://react-project-practice-pi.vercel.app",
     credentials: true,
   }),
 );
